3fc3c869274d89731bbf2c26ea9b19e8ae9663e2
ysuga <ysuga@ysuga.net>
2013-07-20 12:44:18 +0900

    This if first commit. This repository is generated by wasanbon

---
+++ src/CameraViewer.cpp
@@ -0,0 +1,184 @@
+// -*- C++ -*-
+/*!
+ * @file  CameraViewer.cpp
+ * @brief Camera Viewer Component
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "CameraViewer.h"
+#include <opencv2/highgui/highgui.hpp>
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* cameraviewer_spec[] =
+  {
+    "implementation_id", "CameraViewer",
+    "type_name",         "CameraViewer",
+    "description",       "Camera Viewer Component",
+    "version",           "1.0.0",
+    "vendor",            "Sugar Sweet Robotics",
+    "category",          "Experimental",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+CameraViewer::CameraViewer(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_inIn("in", m_in)
+
+    // </rtc-template>
+{
+  m_pImg = NULL;
+}
+
+/*!
+ * @brief destructor
+ */
+CameraViewer::~CameraViewer()
+{
+}
+
+
+
+RTC::ReturnCode_t CameraViewer::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("in", m_inIn);
+  
+  // Set OutPort buffer
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t CameraViewer::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CameraViewer::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CameraViewer::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t CameraViewer::onActivated(RTC::UniqueId ec_id)
+{
+  cvNamedWindow("CapturedImage", CV_WINDOW_AUTOSIZE);
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t CameraViewer::onDeactivated(RTC::UniqueId ec_id)
+{
+  cvReleaseImage(&m_pImg);
+  m_pImg = NULL;
+  cvDestroyWindow("CapturedImage");
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t CameraViewer::onExecute(RTC::UniqueId ec_id) {
+  if (m_inIn.isNew()) {
+    m_inIn.read();
+    if ((m_pImg == NULL) || 
+	(m_in.width != m_pImg->width || m_in.height != m_pImg->height)) {
+      if(m_pImg != NULL) {
+	cvReleaseImage(&m_pImg);
+      }
+      m_pImg = cvCreateImage(cvSize(m_in.width, m_in.height), IPL_DEPTH_8U, 3);
+      
+    }
+    memcpy(m_pImg->imageData, (void*)&(m_in.pixels[0]), m_in.pixels.length());
+    cvShowImage("CapturedImage", m_pImg);
+    cvWaitKey(0);
+  }
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t CameraViewer::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CameraViewer::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CameraViewer::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CameraViewer::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CameraViewer::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void CameraViewerInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(cameraviewer_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<CameraViewer>,
+                             RTC::Delete<CameraViewer>);
+  }
+  
+};
+
+
